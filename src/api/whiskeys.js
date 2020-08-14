7b789ed948cdbb38c13d96636e398d8ff9058152
slawoe <info@slawomirernst.de>
2020-08-14 18:55:18 +0200

    Add more Whiskeys in the API with Philipps help

--- src/api/whiskeys.js
+++ src/api/whiskeys.js
@@ -1,20 +1,27 @@
 export default async function fetchWhiskeys() {
-  const response = await fetch(
-    "https://evening-citadel-85778.herokuapp.com:443/whiskey/"
-  );
-  if (!response.ok) {
-    throw new Error(response);
+  const whiskeyList = [];
+  let i = 1;
+  while (i < 10) {
+    console.log(i);
+    const response = await fetch(
+      `https://evening-citadel-85778.herokuapp.com:443/whiskey/?page=${i}`
+    );
+    if (!response.ok) {
+      throw new Error(response);
+    }
+    const data = await response.json();
+    console.log(data.results);
+    whiskeyList.push(...data.results);
+    i++;
   }
-  const data = await response.json();
-  console.log(data);
-  const riskyWhiskey = data.results.map((whiskey) => ({
+  console.log(whiskeyList);
+  const whiskeys = whiskeyList.map((whiskey) => ({
     title: whiskey.title,
     description: whiskey.description,
     id: whiskey.id,
-    img_url: whiskey.img_url,
     price: whiskey.price,
     rating: whiskey.rating,
     region: whiskey.region,
   }));
-  return riskyWhiskey;
+  return whiskeys;
 }
