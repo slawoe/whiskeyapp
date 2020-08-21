471ce7d13b6c67f137cffb82ebf89aa9ac7aeaab
slawoe <info@slawomirernst.de>
2020-08-21 14:56:52 +0200

    Add router, switches and links to the components

--- src/components/ListItemText.js
+++ src/components/ListItemText.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 
 const ListItemInnerText = styled.div`
   flex-grow: 1;
+  padding: 2px 0;
   & > h2 {
     font-size: 0.85em;
     margin: 0;
@@ -13,14 +14,31 @@ const ListItemInnerText = styled.div`
   }
 `;
 
+const AddButton = styled.button`
+  border: 1px solid black;
+  color: green;
+  border-radius: 2px;
+  margin: 2px;
+`;
+const DeleteButton = styled.button`
+  border: 1px solid black;
+  color: red;
+  border-radius: 2px;
+  margin: 2px;
+`;
+
 function ListItemText({ title, price, region, rating, description }) {
   return (
-    <ListItemInnerText>
-      <h2>{title}</h2>
-      <p>{price}</p>
-      <p>{region}</p>
-      <p>{rating}</p>
-    </ListItemInnerText>
+    <>
+      <ListItemInnerText>
+        <h2>{title}</h2>
+        <p>{price}</p>
+        <p>{region}</p>
+        <p>{rating}</p>
+      </ListItemInnerText>
+      <AddButton>+</AddButton>
+      <DeleteButton>x</DeleteButton>
+    </>
   );
 }
 
